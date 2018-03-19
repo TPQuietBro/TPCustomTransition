15c8029b9ecc606a27fb1b6d677553fede301e29
唐鹏 <tangpeng@icarbonx.com>
2018-03-19 09:08:37 +0800

    Initial Commit

---
+++ TPTransitionAnimations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TPTransitionAnimations
+//
+//  Created by 唐鹏 on 2018/3/19.
+//  Copyright © 2018年 ICX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TPTransitionAnimations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TPTransitionAnimations
+//
+//  Created by 唐鹏 on 2018/3/19.
+//  Copyright © 2018年 ICX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
